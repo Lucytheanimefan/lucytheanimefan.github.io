a71cbdb0e4f1e55bf114406234e62de3637dcd53
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-12-24 09:50:06 -0500

    Almost done with holidaycard

--- script.js
+++
@@ -1,7 +0,0 @@
-var titles = ["Shelter", "Fate Zero", "Psychopass", "Durarara!!", "Madoka Magica"]
-
-function updateDesc(num) {
-    $("#animetitle").empty();
-    $(".description p").empty();
-    $("#animetitle").html(titles[num-1]);
-}
